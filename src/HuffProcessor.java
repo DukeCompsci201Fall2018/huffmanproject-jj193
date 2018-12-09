fe76434edcd934aa4901e95957634a9d7a1d46d4
John Jeong <jj193@duke.edu>
2018-12-08 22:42:51 -0500

    fourth

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -1,123 +1,106 @@
 import java.util.PriorityQueue;
 
-public class HuffProcessor {
+public class HuffProcessor{
+	
 	public static final int BITS_PER_WORD = 8;
 	public static final int BITS_PER_INT = 32;
 	public static final int ALPH_SIZE = (1 << BITS_PER_WORD); 
 	public static final int PSEUDO_EOF = ALPH_SIZE;
 	public static final int HUFF_NUMBER = 0xface8200;
-	public static final int HUFF_TREE  = HUFF_NUMBER | 1;
-
-	private final int myDebugLevel;
 	
-	public static final int DEBUG_HIGH = 4;
-	public static final int DEBUG_LOW = 1;
-	
-	public HuffProcessor() {
-		this(0);
-	}
-	
-	public HuffProcessor(int debug) {
-		myDebugLevel = debug;
-	}
-	private String[] charCodes;
-	private int j;
-	private int[] charCount;
-
 	public void compress(BitInputStream in, BitOutputStream out) {
-		charCount = new int[ALPH_SIZE];
-		j = in.readBits(BITS_PER_WORD);
-		while (j != -1) {
-			charCount[j] = charCount[j] + 1;
-			j = in.readBits(BITS_PER_WORD);
+
+		int[] count = new int[ALPH_SIZE];
+		while(true){
+			int character = in.readBits(BITS_PER_WORD);
+			if(character == -1)
+				break;
+			count[character]++;
 		}
 		in.reset();
 
-		PriorityQueue<HuffNode> pq = new PriorityQueue<HuffNode>();
-		for (int k = 0; k < ALPH_SIZE; k++) {
-			if (charCount[k] != 0) {
-				pq.add(new HuffNode(k, charCount[k]));
+		PriorityQueue<HuffNode> HuffmanTree = new PriorityQueue<HuffNode>();
+		for(int i = 0; i < ALPH_SIZE; i++){
+			if(count[i] != 0){
+				HuffmanTree.add(new HuffNode(i, count[i]));
 			}
 		}
-		pq.add(new HuffNode(PSEUDO_EOF, 0));
-
-		while (pq.size() > 1) {
-			HuffNode left = pq.poll();
-			HuffNode right = pq.poll();
-			pq.add(new HuffNode(-1, right.myWeight + left.myWeight, left, right));
+		HuffmanTree.add(new HuffNode(PSEUDO_EOF, 0));
+		while(HuffmanTree.size() > 1){
+			HuffNode sub1 = HuffmanTree.poll();
+			HuffNode sub2 = HuffmanTree.poll();
+			HuffmanTree.add(new HuffNode(-1, sub1.myWeight+sub2.myWeight, sub1, sub2));
 		}
-		HuffNode myHead = pq.poll();
 
-		charCodes = new String[ALPH_SIZE + 1];
-		extractCodes(myHead, "");
+		String[] codes = new String[ALPH_SIZE+1];
+		HuffNode root = HuffmanTree.poll();
+		extractCodes(root, "", codes);
 
 		out.writeBits(BITS_PER_INT, HUFF_NUMBER);
-		writeHeader(myHead, out);
+		writeHeader(root, out);
 
-		j = in.readBits(BITS_PER_WORD);
-		while (j != -1) {
-			String myCode = charCodes[j];
-			out.writeBits(myCode.length(), Integer.parseInt(myCode, 2));
-			j = in.readBits(BITS_PER_WORD);
+		while(true){
+			int character = in.readBits(BITS_PER_WORD);
+			if(character == -1)
+				break;
+			String code = codes[character];
+			out.writeBits(code.length(), Integer.parseInt(code, 2));
 		}
 
-		String endFile = charCodes[PSEUDO_EOF];
-		out.writeBits(endFile.length(), Integer.parseInt(endFile, 2));
+		String code = codes[PSEUDO_EOF];
+		out.writeBits(code.length(), Integer.parseInt(code, 2));
+	}
+
+	private void extractCodes(HuffNode current, String path, String[] codes) {
+		if(current.myLeft == null && current.myRight == null){
+			codes[current.myValue] = path;
+			return;
+		}
+		extractCodes(current.myLeft, path + 0, codes);
+		extractCodes(current.myRight, path + 1, codes);
+	}
+	
+	private void writeHeader(HuffNode current, BitOutputStream out){
+		if(current.myLeft == null && current.myRight == null){
+			out.writeBits(1, 1);
+			out.writeBits(BITS_PER_WORD + 1, current.myValue);
+			return;
+		}
+		out.writeBits(1, 0);
+		writeHeader(current.myLeft, out);
+		writeHeader(current.myRight, out);
 	}
 
 	public void decompress(BitInputStream in, BitOutputStream out) {
-		if (in.readBits(BITS_PER_INT) != HUFF_NUMBER) {
-			throw new HuffException("NO HUFF NUMBER");
-		}
-		
+		if(in.readBits(BITS_PER_INT) != HUFF_NUMBER)
+			throw new HuffException("HUFF_NUMBER is not presented!");
 		HuffNode root = readHeader(in);
 		HuffNode current = root;
-		j = in.readBits(1);
-		while (j != -1) {
-			if (j == 1) {
+		while(true){
+			int bit = in.readBits(1);
+			if(bit == -1)
+				break;
+			if(bit == 1)
 				current = current.myRight;
-			} else {
+			else 
 				current = current.myLeft;
-			}
-			j = in.readBits(1);
-			if (current.myLeft == null && current.myRight == null) {
-				if (current.myValue == PSEUDO_EOF) {
+			if(current.myLeft == null && current.myRight == null){
+				if(current.myValue == PSEUDO_EOF)
 					return;
-				} else {
-					out.writeBits(8, current.myValue);
-					current = root;
-				}
+				else { 
+					out.writeBits(BITS_PER_WORD, current.myValue);
+				    current = root;
+				    }
 			}
-		}
-		throw new HuffException("PROBLEM WITH PSEUDO_EOF");
+		}	
 	}
-	
-	private void extractCodes(HuffNode current, String path) {
-		if ((current.myLeft == null) && (current.myRight == null)) {
-			charCodes[current.myValue] = path;
-			return;
-		}
-		extractCodes(current.myLeft, path + 0);
-		extractCodes(current.myRight, path + 1);
-	}
-
-	private void writeHeader(HuffNode current, BitOutputStream out) {
-		if ((current.myLeft == null) && (current.myRight == null)) {
-			out.writeBits(1, 1);
-			out.writeBits(9, current.myValue);
-		} else {
-			out.writeBits(1, 0);
-			writeHeader(current.myLeft, out);
-			writeHeader(current.myRight, out);
-		}
-	}
-	private HuffNode readHeader(BitInputStream in) {
-		if (in.readBits(1) == 0) {
+	private HuffNode readHeader(BitInputStream in){
+		if(in.readBits(1) == 0){
 			HuffNode left = readHeader(in);
 			HuffNode right = readHeader(in);
-			return new HuffNode(-1, left.myWeight + right.myWeight, left, right);
+			return new HuffNode(-1, 0, left, right);
 		} else {
-			return new HuffNode(in.readBits(9), 700);
+			return new HuffNode(in.readBits(BITS_PER_WORD+1), 0);
 		}
 	}
 }
@@ -283,7 +266,7 @@ public class HuffProcessor {
 //				if (current.myLeft == null && current.myRight == null) {
 //					if (current.myValue == PSEUDO_EOF) break;
 //					else {
-//						out.writeBits(BITS_PER_WORD+1, current.myValue);
+//						out.writeBits(BITS_PER_WORD, current.myValue);
 //						current = root; // start back after leaf
 //					}
 //				}
